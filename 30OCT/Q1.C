f5113f0ab5afff80a0b814c45db85a70763048ae
Abhishek Kumar Sharma <abhisheksharmacky@gmail.com>
2024-10-30 21:55:46 +0530

    Q1.C

---
+++ 30OCT/Q1.C
@@ -0,0 +1,33 @@
+## Question
+ The program captures and stores student information such as their name, roll number, and marks. It dynamically allocates memory for
+ storing the student's name and displays the collected information after taking inputs.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct student {
+    char *name;
+    int rollno;
+    float marks;
+};
+
+int main() {
+    struct student s1;
+    struct student *p = &s1;
+    p->name = (char*)malloc(30 * sizeof(char));
+    if (p->name == NULL) { 
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+    printf("Enter name: \n");
+    fgets(p->name,30,stdin);
+    printf("Enter Roll Number: \n");
+    scanf("%d",&p->rollno);
+    printf("Enter Marks: \n");
+    scanf("%f",&p->marks);
+    printf("Name: %s Roll Number: %d \nMarks: %.2f\n",p->name,p->rollno,p->marks);
+    free(p->name);
+    return 0;
+
+}
