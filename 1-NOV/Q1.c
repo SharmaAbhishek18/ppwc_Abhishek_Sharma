a994c57aecc021e6a1d685cc472e3268f268fcc8
Abhishek Kumar Sharma <abhisheksharmacky@gmail.com>
2024-11-01 20:42:57 +0530

    Q1.c

---
+++ 1-NOV/Q1.c
@@ -0,0 +1,21 @@
+// QUESTTION: W.A.P to calculate The length of  a String (Without using function)
+
+#include <stdio.h>
+#include <string.h>
+#define MAX 100
+#define NILL ('\0')
+int stringLen(char *str){
+    int len=0;
+    while(str[len]!=NILL){
+        len++;
+    }
+    return len;
+}
+int main(){
+    char s[MAX];
+    printf("Enter a String: ");
+    scanf("%[^\n]s",s);
+    int len=stringLen(s);
+    printf("The length of the String is: %d",len);
+    return 0;
+}
