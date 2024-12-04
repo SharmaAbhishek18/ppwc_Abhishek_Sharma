09979e504c591ae5b4d3d79feaf8a1e5e2990a4d
Abhishek Kumar Sharma <abhisheksharmacky@gmail.com>
2024-12-05 00:21:00 +0530

    Add files via upload

---
+++ Midsem_Solution/Q2c.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void fun(int b[]){
+	printf("%ld\n",sizeof(b)/sizeof(b[0]));
+}
+int main(){
+	int a[5]={2,3};
+	printf("%ld\n",sizeof(a)/sizeof(a[0]));
+	fun(a);
+	return 0;
+}
+/*Output: 
+5
+2
+*/
